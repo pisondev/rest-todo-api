1761301e48bdf3bdb1c07b43f1f3d576ae928cbe
Pison Golda <pison.studyroom@gmail.com>
2025-08-09 00:10:37 +0700

    fix: change param domain.User to string for FindByUsername

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -8,5 +8,5 @@ import (
 
 type UserRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, user domain.User) (domain.User, error)
-	FindByUsername(ctx context.Context, tx *sql.Tx, user domain.User) (domain.User, error)
+	FindByUsername(ctx context.Context, tx *sql.Tx, username string) (domain.User, error)
 }
