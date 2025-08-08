31348b5f7966a3e51bbd25983a93972775fd9c19
Pison Golda <pison.studyroom@gmail.com>
2025-08-08 23:41:33 +0700

    fix: add UserLoginResponse model to send the token only

--- service/user_service.go
+++ service/user_service.go
@@ -7,5 +7,5 @@ import (
 
 type UserService interface {
 	Register(ctx context.Context, req web.UserAuthRequest) (web.UserResponse, error)
-	Login(ctx context.Context, req web.UserAuthRequest) (web.UserResponse, error)
+	Login(ctx context.Context, req web.UserAuthRequest) (web.UserLoginResponse, error)
 }
