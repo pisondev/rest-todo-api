938564317921daf64457e0d3823b08f0dd0fe109
Pison Golda <pison.studyroom@gmail.com>
2025-08-09 19:14:57 +0700

    feat: add middleware & register POST /api/tasks route

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"rest-todo-api/app"
 	"rest-todo-api/controller"
 	"rest-todo-api/exception"
+	"rest-todo-api/middleware"
 	"rest-todo-api/repository"
 	"rest-todo-api/service"
 
@@ -27,12 +28,20 @@ func main() {
 	userService := service.NewUserService(userRepository, db, validate)
 	userController := controller.NewUserController(userService)
 
+	taskRepository := repository.NewTaskRepository()
+	taskService := service.NewTaskService(taskRepository, db, validate)
+	taskController := controller.NewTaskController(taskService)
+
 	server := fiber.New(fiber.Config{
 		ErrorHandler: exception.ErrorHandler,
 	})
 
 	server.Post("/api/register", userController.Register)
 	server.Post("/api/login", userController.Login)
+
+	taskRoutes := server.Group("/api/tasks", middleware.AuthMiddleware())
+
+	taskRoutes.Post("", taskController.Create)
 
 	err = server.Listen(":3000")
 	if err != nil {
