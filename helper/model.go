b7cd742e0a2d818bf90640a44eead81feef1a241
Pison Golda <pison.studyroom@gmail.com>
2025-08-09 19:09:45 +0700

    feat: add ToTaskResponse helper

--- helper/model.go
+++ helper/model.go
@@ -12,3 +12,16 @@ func ToUserResponse(user domain.User) web.UserResponse {
 		CreatedAt: user.CreatedAt,
 	}
 }
+
+func ToTaskResponse(task domain.Task) web.TaskResponse {
+	return web.TaskResponse{
+		ID:          task.ID,
+		UserID:      task.UserID,
+		Title:       task.Title,
+		Description: task.Description,
+		Status:      task.Status,
+		DueDate:     task.DueDate,
+		CreatedAt:   task.CreatedAt,
+		UpdatedAt:   task.UpdatedAt,
+	}
+}
