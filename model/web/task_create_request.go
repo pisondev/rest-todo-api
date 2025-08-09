5c52ae1992ec4f61ab45bba2c6127bb62e74e2f2
Pison Golda <pison.studyroom@gmail.com>
2025-08-09 19:08:29 +0700

    fix: add UserID & fix validation tags

--- model/web/task_create_request.go
+++ model/web/task_create_request.go
@@ -3,7 +3,8 @@ package web
 import "rest-todo-api/model/domain"
 
 type TaskCreateRequest struct {
+	UserID      int                `json:"user_id"`
 	Title       string             `validate:"required,min=1,max=50" json:"title"`
-	Description *string            `validate:"max=200" json:"description"`
+	Description *string            `json:"description"`
 	Status      *domain.TaskStatus `json:"status"`
 }
