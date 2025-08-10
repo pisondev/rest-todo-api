7d8215fa9e8abc8fcd7226e0df5b670a7ba1d0ce
Pison Golda <pison.studyroom@gmail.com>
2025-08-10 15:17:11 +0700

    fix: change DueDate type from *time.Time to *string

--- model/web/task_create_request.go
+++ model/web/task_create_request.go
@@ -2,7 +2,6 @@ package web
 
 import (
 	"rest-todo-api/model/domain"
-	"time"
 )
 
 type TaskCreateRequest struct {
@@ -10,5 +9,5 @@ type TaskCreateRequest struct {
 	Title       string             `validate:"required,min=1,max=50" json:"title"`
 	Description *string            `json:"description"`
 	Status      *domain.TaskStatus `json:"status"`
-	DueDate     *time.Time         `json:"due_date"`
+	DueDate     *string            `json:"due_date"`
 }
