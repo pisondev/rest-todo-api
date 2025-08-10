0a8f40876ef6c3350d646b7968c24c987991560f
Pison Golda <pison.studyroom@gmail.com>
2025-08-10 15:19:34 +0700

    fix: conditional check for DueDate after changing the type in domain & web model

--- helper/model.go
+++ helper/model.go
@@ -14,14 +14,18 @@ func ToUserResponse(user domain.User) web.UserResponse {
 }
 
 func ToTaskResponse(task domain.Task) web.TaskResponse {
-	return web.TaskResponse{
+	response := web.TaskResponse{
 		ID:          task.ID,
 		UserID:      task.UserID,
 		Title:       task.Title,
 		Description: task.Description,
 		Status:      task.Status,
-		DueDate:     task.DueDate,
 		CreatedAt:   task.CreatedAt,
 		UpdatedAt:   task.UpdatedAt,
 	}
+
+	if task.DueDate.Valid {
+		response.DueDate = &task.DueDate.Time
+	}
+	return response
 }
