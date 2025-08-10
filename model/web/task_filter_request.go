7491ca37d077e5bc2f7568388a0c598676da42be
Pison Golda <pison.studyroom@gmail.com>
2025-08-10 18:55:51 +0700

    feat: add TaskFilterRequest web model

--- model/web/task_filter_request.go
+++ model/web/task_filter_request.go
@@ -2,10 +2,9 @@ package web
 
 import (
 	"rest-todo-api/model/domain"
-	"time"
 )
 
 type TaskFilterRequest struct {
 	Status  *domain.TaskStatus `form:"status"`
-	DueDate *time.Time         `form:"due_date"`
+	DueDate *string            `form:"due_date"`
 }
