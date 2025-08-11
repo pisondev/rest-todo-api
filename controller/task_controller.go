b4963926d1a2c8581f526ef5c30200e98147c59d
Pison Golda <pison.studyroom@gmail.com>
2025-08-11 13:31:13 +0700

    feat: add interfaces for Update (task)

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -6,4 +6,5 @@ type TaskController interface {
 	Create(ctx *fiber.Ctx) error
 	FindTasks(ctx *fiber.Ctx) error
 	FindByID(ctx *fiber.Ctx) error
+	Update(ctx *fiber.Ctx) error
 }
--- service/task_service.go
+++ service/task_service.go
@@ -10,4 +10,5 @@ type TaskService interface {
 	Create(ctx context.Context, req web.TaskCreateRequest) (web.TaskResponse, error)
 	FindTasks(ctx context.Context, req web.TaskFilterRequest) ([]web.TaskResponse, error)
 	FindByID(ctx context.Context, taskID int) (web.TaskResponse, error)
+	Update(ctx context.Context, req web.TaskUpdateRequest) (web.TaskResponse, error)
 }
