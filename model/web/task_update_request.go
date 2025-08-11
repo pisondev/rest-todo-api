4ff02f291c721772b25430fe55d8efd06fb28c42
Pison Golda <pison.studyroom@gmail.com>
2025-08-11 15:14:31 +0700

    fix: add validation tag for Title field according to the openapi spec

--- model/web/task_update_request.go
+++ model/web/task_update_request.go
@@ -5,7 +5,7 @@ import "rest-todo-api/model/domain"
 type TaskUpdateRequest struct {
 	ID          int                `json:"id"`
 	UserID      int                `json:"user_id"`
-	Title       *string            `json:"title"`
+	Title       *string            `validate:"omitempty,min=1" json:"title,omitempty"`
 	Description *string            `json:"description"`
 	Status      *domain.TaskStatus `json:"status"`
 }
